6b7f24bdf42308f1767919aca1777f894c3130af
adamkkevin <90542539+adamkkevin@users.noreply.github.com>
2025-02-27 10:19:39 +0700

    edit ljk

--- resources/views/edit-ljk.blade.php
+++ resources/views/edit-ljk.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container" 
-        style="max-width: 600px; margin: auto; padding: 2rem; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); background-color:white;">
-        <h3 style="text-align:center;">Edit Data LJK</h3>
+    <div class="container"
+        style="display: flex; justify-content: center; align-items: center; height:85vh; padding: 2rem; background-color: #FFFFFF;">
+        
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
         <script>
@@ -33,45 +33,62 @@
 
         <!-- Form untuk Mengedit Data -->
         <div class="form-container">
+        <h3 style="text-align:center;">Edit Data LJK</h3>
             <form action="{{ route('daftarljk.update', $ljk->id) }}" method="POST"
                 style="gap: 20px; margin-top:20px; align-items:center; width:100%;">
                 @csrf
 
-                    <div class="form-group">
-                        <input type="text" name="jenis_industri" value="{{ $ljk->jenis_industri }}" placeholder="Jenis Industri" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <input type="text" name="nama_perusahaan" value="{{ $ljk->nama_perusahaan }}" placeholder="Nama Perusahaan" class="form-control" required><br>
-                    </div>
-                
-                    <button type="submit" class="btn btn-primary"
-                        style="background-color: #A91111; color: white; border: none; border-radius: 10px; cursor: pointer; height:40px;">Simpan Perubahan</button>
-                    <a href="{{ route('daftarljk.index') }}" class="btn btn-secondary"
-                        style="border-radius: 10px; background-color: #adb5bd; padding: 12px 24px; color: white; text-align: center; text-decoration: none;">Batal</a>
-                
+                <div class="form-group">
+                    <input type="text" name="jenis_industri" value="{{ $ljk->jenis_industri }}" placeholder="Jenis Industri"
+                        class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <input type="text" name="nama_perusahaan" value="{{ $ljk->nama_perusahaan }}"
+                        placeholder="Nama Perusahaan" class="form-control" required><br>
+                </div>
+
+                <button type="submit" class="btn btn-primary"
+                    style="background-color: #A91111; color: white; border: none; border-radius: 10px; cursor: pointer; height:40px; margin-left:40px;">Simpan
+                    Perubahan</button>
+                <a href="{{ route('daftarljk.index') }}" class="btn btn-secondary"
+                    style="border-radius: 10px; background-color: #adb5bd; padding: 12px 24px; color: white; text-align: center; text-decoration: none;">Batal</a>
+
             </form>
         </div>
     </div>
 
     <style>
         .form-container {
-            margin-top: -10px;
-            width: auto;
+            width: 550px;
             background-color: white;
             display: flex;
             justify-content: center;
-        }
-
-        /* Styling untuk frame form */
-        .form-control {
-            border: 1px solid #ddd;
-            border-radius: 10px;
-            height: 50px;
-            width: 100%;
+            border-radius: 8px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            flex-direction: column;
+            align-items: center;
+            height: 50vh;
+            padding: 20px;
         }
 
         .form-group {
             margin: 10px;
+            margin-left: 20px;
+            display: flex;
+            justify-content: center;
+            /* Menjaga agar elemen berada di tengah */
+            width: 90%;
+            /* Memastikan form-group memanfaatkan ruang yang tersedia */
+        }
+
+        .form-control {
+            border: 1px solid #ddd;
+            border-radius: 10px;
+            height: 50px;
+            width: 90%;
+            /* Menyesuaikan lebar input */
+            display: flex;
+            justify-content: center;
         }
 
         .btn-primary {
